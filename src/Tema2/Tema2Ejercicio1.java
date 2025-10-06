9875b3f3b971d06882961107ec4b0aa468af895c
ApolinarZS <aaapzs23@gmail.com>
2025-10-06 09:13:31 +0200

    Ejercicios 1-2-3

---
+++ src/Tema2/Tema2Ejercicio1.java
@@ -0,0 +1,22 @@
+package Tema2;
+
+import java.util.Scanner;
+
+public class Tema2Ejercicio1 {
+    public static void main (String[] args){
+
+        //Le pido el valor del lado de cuadrado al usuario y creo la variable"\\
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el valor del lado del cuadrado");
+        double lado = scanner.nextDouble();
+
+        //Calculamos el área y el perímetro en dos nuevas variables\\
+        double area = lado*lado;
+        double per = 4*lado;
+
+        //Mostramos por pantalla el resultado\\
+        System.out.println("Siendo " +lado+ " el valor del lado, el área y perímetro del cuadrado son " +area+" y " +per+ " respectivamente.");
+
+        scanner.close();
+    }
+}
